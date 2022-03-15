17e039201f7821308a14c2b4cbe219db60692490
Philippa Rubin <philippa.rubin@stfc.ac.uk>
2022-03-15 11:00:28 +0000

    Added increasing depth comparison tests

--- tests/test_giles_tensor.cpp
+++ tests/test_giles_tensor.cpp
@@ -47,7 +47,349 @@ TEST(test_giles_tensor, fixed_mul_identity) {
     EXPECT_EQ(other*identity - other, zero);
 }
 
-TEST(test_giles_tensor, integer_giles_tensor_simple_tensor_compare_multiplication)
+
+////////////////////// INCREASING DEPTH TESTS ///////////////////////
+
+// TODO: Remove the copy and pasted code in these tests
+
+TEST(test_giles_tensor, depth_1_only_giles_tensor_simple_tensor_compare_multiplication)
+{
+    constexpr unsigned width = 5;
+    constexpr unsigned depth = 5;
+
+    constexpr unsigned depth_to_check = 1;
+
+    // -- lhs -- //
+
+    // giles tensor lhs
+
+    giles_template_tensor<width, depth> giles_lhs;
+
+    for (size_t i = 0; i < tensor_alg_size(width, depth); ++i) {
+        giles_lhs[i] = double(i);
+    }
+
+    // std::cout << "giles_lhs=" << giles_lhs << std::endl;
+
+    // simple tensor lhs
+
+    simple_template_tensor<width, depth> simple_lhs;
+
+    for (size_t i = 0; i < tensor_alg_size(width, depth); ++i) {
+        simple_lhs[i] = double(i);
+    }
+
+    // std::cout << "simple_lhs=" << simple_lhs << std::endl;
+
+    // -- rhs -- //
+
+    // giles tensor rhs
+
+    giles_template_tensor<width, depth> giles_rhs;
+
+    for (size_t i = 0; i < tensor_alg_size(width, depth); ++i) {
+        giles_rhs[i] = double(i);
+    }
+
+    // std::cout << "giles_rhs=" << giles_rhs << std::endl;
+
+    // simple tensor rhs
+
+    simple_template_tensor<width, depth> simple_rhs;
+
+    for (size_t i = 0; i < tensor_alg_size(width, depth); ++i) {
+        simple_rhs[i] = double(i);
+    }
+
+    // std::cout << "simple_rhs=" << simple_rhs << std::endl;
+
+    // ------- Multipy -------- //
+
+    giles_template_tensor<width, depth> giles_result = giles_lhs*giles_rhs;
+
+    simple_template_tensor<width, depth> simple_result = simple_lhs*simple_rhs;
+
+    // ------- Compare --------- //
+
+    for (size_t i = tensor_alg_size(width, depth_to_check-1); i < tensor_alg_size(width, depth_to_check); ++i) 
+    {
+
+        EXPECT_EQ(giles_result[i], simple_result[i]) << "Multiplication result differs at index " << i;
+    }
+
+}
+
+TEST(test_giles_tensor, depth_2_only_giles_tensor_simple_tensor_compare_multiplication)
+{
+    constexpr unsigned width = 5;
+    constexpr unsigned depth = 5;
+
+    constexpr unsigned depth_to_check = 2;
+
+    // -- lhs -- //
+
+    // giles tensor lhs
+
+    giles_template_tensor<width, depth> giles_lhs;
+
+    for (size_t i = 0; i < tensor_alg_size(width, depth); ++i) {
+        giles_lhs[i] = double(i);
+    }
+
+    // std::cout << "giles_lhs=" << giles_lhs << std::endl;
+
+    // simple tensor lhs
+
+    simple_template_tensor<width, depth> simple_lhs;
+
+    for (size_t i = 0; i < tensor_alg_size(width, depth); ++i) {
+        simple_lhs[i] = double(i);
+    }
+
+    // std::cout << "simple_lhs=" << simple_lhs << std::endl;
+
+    // -- rhs -- //
+
+    // giles tensor rhs
+
+    giles_template_tensor<width, depth> giles_rhs;
+
+    for (size_t i = 0; i < tensor_alg_size(width, depth); ++i) {
+        giles_rhs[i] = double(i);
+    }
+
+    // std::cout << "giles_rhs=" << giles_rhs << std::endl;
+
+    // simple tensor rhs
+
+    simple_template_tensor<width, depth> simple_rhs;
+
+    for (size_t i = 0; i < tensor_alg_size(width, depth); ++i) {
+        simple_rhs[i] = double(i);
+    }
+
+    // std::cout << "simple_rhs=" << simple_rhs << std::endl;
+
+    // ------- Multipy -------- //
+
+    giles_template_tensor<width, depth> giles_result = giles_lhs*giles_rhs;
+
+    simple_template_tensor<width, depth> simple_result = simple_lhs*simple_rhs;
+
+    // ------- Compare --------- //
+
+    for (size_t i = tensor_alg_size(width, depth_to_check-1); i < tensor_alg_size(width, depth_to_check); ++i)
+    {
+
+        EXPECT_EQ(giles_result[i], simple_result[i]) << "Multiplication result differs at index " << i;
+    }
+
+}
+
+TEST(test_giles_tensor, depth_3_only_giles_tensor_simple_tensor_compare_multiplication)
+{
+    constexpr unsigned width = 5;
+    constexpr unsigned depth = 5;
+
+    constexpr unsigned depth_to_check = 3;
+
+    // -- lhs -- //
+
+    // giles tensor lhs
+
+    giles_template_tensor<width, depth> giles_lhs;
+
+    for (size_t i = 0; i < tensor_alg_size(width, depth); ++i) {
+        giles_lhs[i] = double(i);
+    }
+
+    // std::cout << "giles_lhs=" << giles_lhs << std::endl;
+
+    // simple tensor lhs
+
+    simple_template_tensor<width, depth> simple_lhs;
+
+    for (size_t i = 0; i < tensor_alg_size(width, depth); ++i) {
+        simple_lhs[i] = double(i);
+    }
+
+    // std::cout << "simple_lhs=" << simple_lhs << std::endl;
+
+    // -- rhs -- //
+
+    // giles tensor rhs
+
+    giles_template_tensor<width, depth> giles_rhs;
+
+    for (size_t i = 0; i < tensor_alg_size(width, depth); ++i) {
+        giles_rhs[i] = double(i);
+    }
+
+    // std::cout << "giles_rhs=" << giles_rhs << std::endl;
+
+    // simple tensor rhs
+
+    simple_template_tensor<width, depth> simple_rhs;
+
+    for (size_t i = 0; i < tensor_alg_size(width, depth); ++i) {
+        simple_rhs[i] = double(i);
+    }
+
+    // std::cout << "simple_rhs=" << simple_rhs << std::endl;
+
+    // ------- Multipy -------- //
+
+    giles_template_tensor<width, depth> giles_result = giles_lhs*giles_rhs;
+
+    simple_template_tensor<width, depth> simple_result = simple_lhs*simple_rhs;
+
+    // ------- Compare --------- //
+
+    for (size_t i = tensor_alg_size(width, depth_to_check-1); i < tensor_alg_size(width, depth_to_check); ++i)
+    {
+
+        EXPECT_EQ(giles_result[i], simple_result[i]) << "Multiplication result differs at index " << i;
+    }
+
+}
+
+TEST(test_giles_tensor, depth_4_only_giles_tensor_simple_tensor_compare_multiplication)
+{
+    constexpr unsigned width = 5;
+    constexpr unsigned depth = 5;
+
+    constexpr unsigned depth_to_check = 4;
+
+    // -- lhs -- //
+
+    // giles tensor lhs
+
+    giles_template_tensor<width, depth> giles_lhs;
+
+    for (size_t i = 0; i < tensor_alg_size(width, depth); ++i) {
+        giles_lhs[i] = double(i);
+    }
+
+    // std::cout << "giles_lhs=" << giles_lhs << std::endl;
+
+    // simple tensor lhs
+
+    simple_template_tensor<width, depth> simple_lhs;
+
+    for (size_t i = 0; i < tensor_alg_size(width, depth); ++i) {
+        simple_lhs[i] = double(i);
+    }
+
+    // std::cout << "simple_lhs=" << simple_lhs << std::endl;
+
+    // -- rhs -- //
+
+    // giles tensor rhs
+
+    giles_template_tensor<width, depth> giles_rhs;
+
+    for (size_t i = 0; i < tensor_alg_size(width, depth); ++i) {
+        giles_rhs[i] = double(i);
+    }
+
+    // std::cout << "giles_rhs=" << giles_rhs << std::endl;
+
+    // simple tensor rhs
+
+    simple_template_tensor<width, depth> simple_rhs;
+
+    for (size_t i = 0; i < tensor_alg_size(width, depth); ++i) {
+        simple_rhs[i] = double(i);
+    }
+
+    // std::cout << "simple_rhs=" << simple_rhs << std::endl;
+
+    // ------- Multipy -------- //
+
+    giles_template_tensor<width, depth> giles_result = giles_lhs*giles_rhs;
+
+    simple_template_tensor<width, depth> simple_result = simple_lhs*simple_rhs;
+
+    // ------- Compare --------- //
+
+    for (size_t i = tensor_alg_size(width, depth_to_check-1); i < tensor_alg_size(width, depth_to_check); ++i)
+    {
+
+        EXPECT_EQ(giles_result[i], simple_result[i]) << "Multiplication result differs at index " << i;
+    }
+
+}
+
+TEST(test_giles_tensor, depth_5_only_giles_tensor_simple_tensor_compare_multiplication)
+{
+    constexpr unsigned width = 5;
+    constexpr unsigned depth = 5;
+
+    constexpr unsigned depth_to_check = 5;
+
+    // -- lhs -- //
+
+    // giles tensor lhs
+
+    giles_template_tensor<width, depth> giles_lhs;
+
+    for (size_t i = 0; i < tensor_alg_size(width, depth); ++i) {
+        giles_lhs[i] = double(i);
+    }
+
+    // std::cout << "giles_lhs=" << giles_lhs << std::endl;
+
+    // simple tensor lhs
+
+    simple_template_tensor<width, depth> simple_lhs;
+
+    for (size_t i = 0; i < tensor_alg_size(width, depth); ++i) {
+        simple_lhs[i] = double(i);
+    }
+
+    // std::cout << "simple_lhs=" << simple_lhs << std::endl;
+
+    // -- rhs -- //
+
+    // giles tensor rhs
+
+    giles_template_tensor<width, depth> giles_rhs;
+
+    for (size_t i = 0; i < tensor_alg_size(width, depth); ++i) {
+        giles_rhs[i] = double(i);
+    }
+
+    // std::cout << "giles_rhs=" << giles_rhs << std::endl;
+
+    // simple tensor rhs
+
+    simple_template_tensor<width, depth> simple_rhs;
+
+    for (size_t i = 0; i < tensor_alg_size(width, depth); ++i) {
+        simple_rhs[i] = double(i);
+    }
+
+    // std::cout << "simple_rhs=" << simple_rhs << std::endl;
+
+    // ------- Multipy -------- //
+
+    giles_template_tensor<width, depth> giles_result = giles_lhs*giles_rhs;
+
+    simple_template_tensor<width, depth> simple_result = simple_lhs*simple_rhs;
+
+    // ------- Compare --------- //
+
+    for (size_t i = tensor_alg_size(width, depth_to_check-1); i < tensor_alg_size(width, depth_to_check); ++i)
+    {
+
+        EXPECT_EQ(giles_result[i], simple_result[i]) << "Multiplication result differs at index " << i;
+    }
+
+}
+
+////////////////////// FULL DEPTH TENSOR TESTS ///////////////////////
+
+TEST(test_giles_tensor, full_depth_giles_tensor_simple_tensor_compare_multiplication)
 {
     constexpr unsigned width = 5;
     constexpr unsigned depth = 5;
